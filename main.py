d9b23f8cb85e5079781f3327e94ee1338e38246d
Kirill dev <educatedperson004@gmail.com>
2023-02-19 20:41:07 +0300

    remove db and add keyboard

--- main.py
+++ main.py
@@ -1,88 +1,50 @@
 from aiogram import Bot, Dispatcher, executor, types
 import datetime
 import requests
-from aiogram.types import ContentType, Message, InputFile
-import modules.messages
+import modules.messages, modules.config
+from modules.keyboard import keyboad
 
-
-
-API_TOKEN = '6083003984:AAHXRKN8zxyBlyLMjJkAgoD99RytkijFs7o'
-bot = Bot(token=API_TOKEN)
+bot = Bot(modules.config.BOT_TOKEN)
 dp = Dispatcher(bot)
-openweather_token = 'baf5502c93346631aa40f65e1b5e85b3'
 
-#1
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-	await message.reply(modules.messages.START)
+	await message.reply(modules.messages.START,
+		     reply_markup=keyboad)
 
-@dp.message_handler(commands=['help'])
+@dp.message_handler(lambda message: message.text == 'Помощь бродяге')
 async def send_help(message: types.Message):
 	await message.reply(modules.messages.HELP)
 
 
-
-
-
-
-
-
-
-
-@dp.message_handler(commands=['weather2'])
+@dp.message_handler(lambda message: message.text == 'Покажи погоду')
 async def weather_get(message: types.Message):
-	try:
-		await bot.send_message(message.from_user.id, "город")
-		r = requests.get(
-			f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={openweather_token}&units=metric"
-			
-			)
-		data = r.json()
-
-		temp_cur = data["main"]["temp"]
-		humidity_cur = data["main"]["humidity"]
-		wind_cur = data["wind"]["speed"]
-		pressure_cur = data["main"]["pressure"]
-		sunrise_cur = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
-		sunset_cur = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
-		sunr_minus_suns = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(data["sys"]["sunrise"]) 
-
-
-
+	r = requests.get(
+		f"https://api.openweathermap.org/data/2.5/weather?q=rostov&appid={modules.config.OPENWEATHER_TOKEN}&units=metric"
 		
-		await message.reply(
-			f"***{datetime.datetime.now().strftime('%d.%m.%Y - %H:%M')}***\n"
-			f"Температура: {temp_cur} °C\n"
-			f"Влажность воздуха: {humidity_cur}%\n"
-			f"Скорость ветра: {wind_cur} m/s\n"
-			f"Атмосферное давление: {pressure_cur} мм.рт.ст\n"
-			f"Восход: {sunrise_cur}\n"
-			f"Заход: {sunset_cur}\n"
-			f"Дневное время: {sunr_minus_suns}\n"
-			f"Хорошего дня!\n"
-			)
+		)
+	data = r.json()
 
-	except:
-		await message.reply("Проверьте название!")
+	temp_cur = data["main"]["temp"]
+	humidity_cur = data["main"]["humidity"]
+	wind_cur = data["wind"]["speed"]
+	pressure_cur = data["main"]["pressure"]
+	sunrise_cur = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+	sunset_cur = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+	sunr_minus_suns = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(data["sys"]["sunrise"]) 
 
-
-
-
-#@dp.message_handler(content_types=ContentType.PHOTO)
-#async def photo_send_id(message: Message):
-	#await message.reply(message.photo[-1].file_id)
-
-
-#@dp.message_handler(commands='photo')
-##async def photo_send(message: Message):
-	#chat_id = message.from_user.id
-	#photo_file_id = 'AgACAgIAAxkBAAIBQmPwl2I8g8BQRKbL6vOwPoFRs0aZAALWwjEbbTuJS4Dx70DXZoUrAQADAgADeQADLgQ'
-
-
-	#await dp.bot.photo_send(chat_id=chat_id, photo_send_id)
-
-
+	await message.reply(
+		f"***{datetime.datetime.now().strftime('%d.%m.%Y - %H:%M')}***\n"
+		f"Температура: {temp_cur} °C\n"
+		f"Влажность воздуха: {humidity_cur}%\n"
+		f"Скорость ветра: {wind_cur} m/s\n"
+		f"Атмосферное давление: {pressure_cur} мм.рт.ст\n"
+		f"Восход: {sunrise_cur}\n"
+		f"Заход: {sunset_cur}\n"
+		f"Дневное время: {sunr_minus_suns}\n"
+		f"Хорошего дня!\n"
+		)
 
 
 if __name__ == '__main__':
-	executor.start_polling(dp)	
+	executor.start_polling(dp, skip_updates=True)	
---
+++ modules/keyboard.py
@@ -0,0 +1,4 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+keyboad = ReplyKeyboardMarkup(resize_keyboard=True)
+keyboad.add(KeyboardButton('Покажи погоду'))
